bc1205554676b5dddd63709db50043001f7fcc3c
esamanev@470-amocrm <esamanev@team.amocrm.com>
2023-03-10 13:15:16 +0300

    Исправленный вариант

--- internal/database.go
+++ internal/database.go
@@ -9,7 +9,7 @@ type Account struct {
 	RefreshToken string
 	Expires      time.Time
 	AccountID    int
-	Integration  Integration
+	Integration  map[string]Integration
 }
 
 type Integration struct {
@@ -17,40 +17,4 @@ type Integration struct {
 	ClientID           string
 	RedirectURL        string
 	AuthenticationCode string
-}
-
-type Repository struct {
-	accounts map[int]Account
-}
-
-func NewRepository() *Repository {
-	return &Repository{
-		accounts: make(map[int]Account),
-	}
-}
-
-func (r *Repository) AddAccount(account Account) {
-	r.accounts[account.AccountID] = account
-}
-
-func (r *Repository) AddIntegration(accountID int, integration Integration) {
-	account, ok := r.accounts[accountID]
-	if !ok {
-		return
-	}
-	account.Integration = integration
-	r.accounts[accountID] = account
-}
-
-func (r *Repository) GetAccount(accountID int) (Account, bool) {
-	account, ok := r.accounts[accountID]
-	return account, ok
-}
-
-func (r *Repository) GetAllAccounts() []Account {
-	accounts := make([]Account, 0, len(r.accounts))
-	for _, account := range r.accounts {
-		accounts = append(accounts, account)
-	}
-	return accounts
 }
--- pkg/functions.go
+++ pkg/functions.go
@@ -1,35 +1,40 @@
 package pkg
 
+import "apitraning/internal"
+
 type Repository struct {
-	accounts map[int]Account
+	accounts map[int]internal.Account
 }
 
 func NewRepository() *Repository {
 	return &Repository{
-		accounts: make(map[int]Account),
+		accounts: make(map[int]internal.Account),
 	}
 }
 
-func (r *Repository) AddAccount(account Account) {
+func (r *Repository) AddAccount(account internal.Account) {
 	r.accounts[account.AccountID] = account
 }
 
-func (r *Repository) AddIntegration(accountID int, integration Integration) {
+func (r *Repository) AddIntegration(accountID int, integration internal.Integration) {
 	account, ok := r.accounts[accountID]
 	if !ok {
 		return
 	}
-	account.Integration = integration
+	account.Integration[integration.ClientID] = integration
 	r.accounts[accountID] = account
 }
 
-func (r *Repository) GetAccount(accountID int) (Account, bool) {
+func (r *Repository) GetAccountIntegrations(accountID int) map[string]internal.Integration {
 	account, ok := r.accounts[accountID]
-	return account, ok
+	if !ok {
+		return nil
+	}
+	return account.Integration
 }
 
-func (r *Repository) GetAllAccounts() []Account {
-	accounts := make([]Account, 0, len(r.accounts))
+func (r *Repository) GetAllAccounts() []internal.Account {
+	accounts := make([]internal.Account, 0, len(r.accounts))
 	for _, account := range r.accounts {
 		accounts = append(accounts, account)
 	}
